f70420b9ae7cca37a3acbd5b6b3bbf77a5d3bbdc
MaheshKumarGM-tech <73214700+MaheshKumarGM-tech@users.noreply.github.com>
2021-01-06 23:48:23 +0530

    initial commit

---
+++ WebApplication1/ClassLib/datalayer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ClassLib
+{
+    public class datalayer
+    {
+        public string getfitnesspaldata(string userid)
+        {
+            string connstr = ConfigurationManager.ConnectionStrings["localdbconnection"].ConnectionString;
+            DataTable dt = new DataTable("fitnesspaldata");
+            using (SqlConnection conn = new SqlConnection(connstr))
+            {
+                SqlCommand sqlComm = new SqlCommand("uspMyFitnessPal", conn);
+                sqlComm.Parameters.AddWithValue("@userName", userid);
+                sqlComm.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = sqlComm;
+
+                da.Fill(dt);
+            }
+            System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+            Dictionary<string, object> row;
+            foreach (DataRow dr in dt.Rows)
+            {
+                row = new Dictionary<string, object>();
+                foreach (DataColumn col in dt.Columns)
+                {
+                    row.Add(col.ColumnName, dr[col]);
+                }
+                rows.Add(row);
+            }
+            return serializer.Serialize(rows);
+        }
+
+    }
+}
---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using WebApplication1.ClassLib;
+
+namespace WebApplication1.Controllers
+{
+    public class UserObj
+    {
+        public string MyFitnessPalUserName { get; set; }
+    }
+
+    public class ValuesController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        //GET api/values
+        public HttpResponseMessage Get([FromBody] UserObj user)
+        {
+            string userid = user.MyFitnessPalUserName;
+            datalayer dl = new datalayer();
+            var res = dl.getfitnesspaldata(userid);
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(res.ToString(), Encoding.UTF8, "application/json");
+            return response;
+        }
+
+    }
+}
